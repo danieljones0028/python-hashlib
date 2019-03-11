cb9592d3b32b9eef862dc31af7ee84da1554b826
Daniel Jones <danieljones028@gmail.com>
2019-03-11 14:34:25 -0300

    ok

--- CreatePasswordFromListHash.py
+++ CreatePasswordFromListHash.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3.5
 # Docs
 # https://docs.python.org/3.5/library/hashlib.html#module-hashlib
 
@@ -7,6 +7,5 @@ import hashlib
 users = open('path_to_file', 'r').read().split('\n')
 
 for user in users:
-  if user == user:
-    password = hashlib.md5(user.encode('utf-8')).hexdigest()
-    print(f"user={user} password={password}")
+  password = hashlib.md5(user.encode('utf-8')).hexdigest()
+  print(f"user={user} passwd={password}")
